1edb5aed4c0ed17672bbd05acffcc357a7c3ec11
tariqmuh <khurram_tariq@live.ca>
2014-03-05 14:52:43 -0500

    grayscale works

--- workspace/YCbCr_Convert/src/ycbcr_convert.c
+++ workspace/YCbCr_Convert/src/ycbcr_convert.c
@@ -60,47 +60,23 @@ int main() {
 
 
 	int f_pixel, /*s_pixel,*/ pixels, f_grayscale, s_grayscale, grayscale;
-	int f_red, f_green, f_blue;
-	int s_red, s_green, s_blue;
 	while(1){
 
 		//for(j = 0; j<480 ; j++){
 			//j = 0;
 			for(i=0; i<(320*480) ; i++){
-#ifdef AVERAGE
-				pixels = ddr_addr[i];
-				//f_pixel = pixels & 0x0000FFFF;
-				f_red = ((pixels >> 11) & 0x0000001F) << 3;
-				f_green = ((pixels >> 5) & 0x0000003F) << 2;
-				f_blue = ((pixels) & 0x0000001F) << 3;
-				f_grayscale = ( (f_red) + (f_green) + (f_blue) ) / 3;
-				f_grayscale = ( ((f_grayscale >> 3) & 0x0000001F)  << 11) | ( ((f_grayscale >> 2) & 0x0000003F) << 5) | ( ((f_grayscale >> 3) & 0x0000001F) );
-
-				//s_pixel = pixels >> 16;
-				s_red = ((pixels >> 27) & 0x0000001F) << 3;
-				s_green = ((pixels >> 21) & 0x0000003F) << 2;
-				s_blue = ((pixels >> 16) & 0x0000001F) << 3;
-				s_grayscale = ( (s_red) + (s_green) + (s_blue) ) / 3;
-				s_grayscale = ( ((s_grayscale >> 3) & 0x0000001F)  << 27) | ( ((s_grayscale >> 2) & 0x0000003F) << 21) | ( ((s_grayscale >> 3) & 0x0000001F) << 16);
-
-				//f_grayscale = ( /*Red*/ (f_pixel >> 11) + /*Green*/ ( (f_pixel >> 6) & 0x0000001F) + /*Blue*/ ( f_pixel & 0x0000001F) ) / 3;
-//				s_grayscale = ( /*Red*/ (pixels >> 27) + /*Green*/ ( (pixels >> 22) & 0x0000001F) + /*Blue*/ (( pixels >> 16) & 0x0000001F) ) / 3;
-				//grayscale = (s_grayscale << 27) | (s_grayscale << 22) | (s_grayscale << 16) | (f_grayscale << 11) | (f_grayscale << 6) | (f_grayscale);
-				grayscale = s_grayscale | f_grayscale;
-				ddr_addr2[i] = grayscale;
-				//ddr_addr2[i] = pixels;
-#elif defined(LUMINOSITY)
 				pixels = ddr_addr[i];
 				f_pixel = pixels & 0x0000FFFF;
 				//s_pixel = pixels >> 16;
-				f_grayscale = /*Red*/ (f_pixel >> 11) * 0.21 + /*Green*/ ( (f_pixel >> 6) & 0x0000003F) * 0.71 + /*Blue*/ ( f_pixel & 0x0000001F) * 0.07;
-				s_grayscale = /*Red*/ (pixels >> 27) * 0.21 + /*Green*/ ( (pixels >> 22) & 0x0000003F) * 0.71 + /*Blue*/ (( pixels >> 16) & 0x0000001F) * 0.07;
+				f_grayscale = ( /*Red*/ (f_pixel >> 11) + /*Green*/ ( (f_pixel >> 6) & 0x0000003F) + /*Blue*/ ( f_pixel & 0x0000001F) ) / 3;
+				s_grayscale = ( /*Red*/ (pixels >> 27) + /*Green*/ ( (pixels >> 22) & 0x0000003F) + /*Blue*/ (( pixels >> 16) & 0x0000001F) ) / 3;
 				grayscale = (s_grayscale << 27) | (s_grayscale << 22) | (s_grayscale << 16) | (f_grayscale << 11) | (f_grayscale << 6) | (f_grayscale);
 				ddr_addr2[i] = grayscale;
-#endif
-//				printf ("i: %d, f_grayscale: 0x%X, s_grayscale: 0x%X\n\r", i, f_grayscale, s_grayscale);
-//				printf ("i: %d, pixels: 0x%X, grayscale: 0x%X\n\r", i, pixels, grayscale);
-//				printf ("ddr_addr2: 0x%X\n\r", ddr_addr2[i]);
+				//ddr_addr2[i] = pixels;
+
+			//	printf ("i: %d, f_grayscale: 0x%X, s_grayscale: 0x%X\n\r", i, f_grayscale, s_grayscale);
+		//		printf ("i: %d, pixels: 0x%X, grayscale: 0x%X\n\r", i, pixels, grayscale);
+			//	printf ("ddr_addr2: 0x%X\n\r", ddr_addr2[i]);
 			}
 		//}
 	}
